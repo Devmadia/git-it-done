5853faaa21925ecbbd8726e206b9130c1a3cf75f
Devmadia <agathaphan@icloud.com>
2020-06-23 00:18:49 -0500

    generate stylized link displays for repo issues and on-click display in a new browser tab

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,6 @@
+// reference to the issues container
+var issueContainerEl = document.querySelector("#issues-container");
+
 // function that will take in a repo name as a parameter
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -19,13 +22,42 @@ var getRepoIssues = function(repo) {
 
 // function for the repo issues
 var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
+        //  links to the full issue on GitHub
         issueEl.setAttribute("href", issues[i].html_url);
+        // open the link in a new tab instead of replacing the current webpage
         issueEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+        // append to container
+        issueEl.appendChild(typeEl);
+        issueContainerEl.appendChild(issueEl);
     }
+
+             
 };
 
 getRepoIssues("facebook/react");
